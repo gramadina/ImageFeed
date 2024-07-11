f57f14ed5cdb791bc7d4aa84a388313762d085e6
Дарья Гранина <dashuny14@gmail.com>
2024-07-11 15:26:35 +0400

    delegate

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 final class AuthViewController: UIViewController {
+    private let showWebViewSegueIdentifier = "ShowWebView"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackButton()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewSegueIdentifier {
+            guard let webViewViewController = segue.destination as? WebViewViewController 
+            else { fatalError("Failed to prepare for \(showWebViewSegueIdentifier)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
     
     private func configureBackButton() {
@@ -19,5 +30,15 @@ final class AuthViewController: UIViewController {
         navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black")
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+    
+    func webViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
     }
 }
